6f60aea2fd55d51d4a76bc97473d712a52ea1c8d
Pheonixia <fatihatulkhaer@gmail.com>
2024-12-29 18:42:52 +0100

    Delete Backend directory

--- Backend/middlewares/authmiddleware.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.isAuthenticated = (req, res, next) => {
-  if (!req.session.userId) {
-    return res.status(401).send("Unauthorized! Please log in.");
-  }
-  next(); // User is authenticated, proceed to the next middleware or route handler
-};
