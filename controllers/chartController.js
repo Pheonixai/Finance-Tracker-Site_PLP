4206796f1aad832c235122bd922498595dec7440
Pheonixia <fatihatulkhaer@gmail.com>
2024-12-30 19:41:12 +0100

    Update chartController.js

--- controllers/chartController.js
+++ controllers/chartController.js
@@ -12,7 +12,7 @@
      password: process.env.DB_PASSWORD,
      database: process.env.DB_NAME,
      waitForConnections: true,
-     connectionLimit: 10,
+     connectionLimit: 5,
      queueLimit: 0
 });
 
